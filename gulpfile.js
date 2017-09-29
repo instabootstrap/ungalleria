f61a6c133e73059bae2c6534e5e704697671159a
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2017-09-29 08:39:52 +0700

    Fix navbar menu in mobile

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('html', function() {
 });
 
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("dist/js/"))
         .pipe(browserSync.stream());
 });
